d7194daf2325f57eeddf5ff7bce3481b31628a27
Jasper935 <102736176+Jasper935@users.noreply.github.com>
2022-08-06 18:18:29 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,18 @@
-import { Component } from 'react';
 import { Filter } from './Filter/Filter';
 import { Form } from '../components/Form/Form';
 import { Section } from './Section/Section';
 
 import { ContactsList } from './ContactsList/ContactsList';
-export class App extends Component {
-  render() {
-    return (
-      <>
-        <Section title={'PhoneBook'}>
-          <Form />
-        </Section>
-        <Section title={'Contacts'}>
-          <Filter />
-          <ContactsList />
-        </Section>
-      </>
-    );
-  }
-}
+export const App = () => {
+  return (
+    <>
+      <Section title={'PhoneBook'}>
+        <Form />
+      </Section>
+      <Section title={'Contacts'}>
+        <Filter />
+        <ContactsList />
+      </Section>
+    </>
+  );
+};
